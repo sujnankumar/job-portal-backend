c5fe97248e3fe90d2190b1f0bafdca5f48671fde
sujalmh9@gmail.com <sujalmh>
2025-04-30 21:30:43 +0530

    CORS changes

--- app/main.py
+++ app/main.py
@@ -19,7 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://job-portal-frontend-n81l.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
